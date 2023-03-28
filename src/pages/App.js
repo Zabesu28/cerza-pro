a530d095fb36e68c361e05be38d0910b25690204
DNOLLE27 <damien.nolle@stadjutor.com>
2023-03-28 11:41:34 +0200

    <feature>(creationComptes) : début interface création comptes

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,6 +4,7 @@ import React, { Component, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthAdmin from "./AuthAdmin";
 import AuthUser from "./AuthUser";
+import gestionComptes from "./gestionComptes";
 
 function App() {
   const [showNav, setShowNav] = useState(false);
@@ -16,6 +17,7 @@ function App() {
           <Route path="home" element={<Home />} />
           <Route path="authAdmin" element={<AuthAdmin />} />
           <Route path="authUser" element={<AuthUser />} />
+          <Route path="gestionComptes" element={<gestionComptes />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/gestionComptes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/gestionComptes.css";
+import Navbar from "../components/Navbar";
+
+const gestionComptes = () => {
+  return (
+    <div>
+      <Navbar />
+      <p>test</p>
+    </div>
+  );
+};
+
+export default gestionComptes;
