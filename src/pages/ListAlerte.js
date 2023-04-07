f50b8b95fd97f95c76078fe2880c7f830a63b78b
Zabesu <romain.zabeth@gmail.com>
2023-04-07 14:19:21 +0200

    <feature>(gestion-alertes): fin css list

--- src/pages/ListAlerte.js
+++ src/pages/ListAlerte.js
@@ -19,6 +19,9 @@ import Autocomplete from '@mui/material/Autocomplete';
 import Stack from '@mui/material/Stack';
 import Checkbox from '@mui/material/Checkbox';
 import MenuItem from '@mui/material/MenuItem';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import IconButton from '@mui/material/IconButton';
 
 
 
@@ -34,18 +37,18 @@ const ListAlerte = () => {
 
     }
     
-     const Delete = (idAlerte) => {
+    const Delete = (idAlerte) => {
         //1 copie
         console.log(idAlerte);
         fetch('http://localhost:4000/DeleteAlerte/'+idAlerte, { method: 'DELETE' }) 
         alert("L'alerte a bien été supprimé"); 
-        window.location.href = 'http://localhost:4000/gestionAlerte'
     }
+
     const Update = (idAlerte) => {
       //1 copie
       console.log(idAlerte + "c'est la modif");
       fetch('http://localhost:3000/modifAlerte/'+idAlerte)   
-     }
+    }
 
      function convertDate(date){
       const dater = new Date(date);
@@ -88,8 +91,7 @@ const ListAlerte = () => {
 
     return (
         <div>
-          <h1>Gestion des Alertes</h1>
-          <a href="/addAlerte"><Button variant="contained" >Créer une alerte</Button></a>
+          <h1 class="center">Gestion des Alertes</h1>
           <TextField name="idNiveauAlerte" onChange={e => 
             idNiveauchange(e.target.value)
             }
@@ -101,7 +103,9 @@ const ListAlerte = () => {
             <MenuItem value={0}>Tout</MenuItem>
              {(niveau.map(unNiveau => <MenuItem value={unNiveau.idNiveau}>{unNiveau.libelleNiveau}</MenuItem>))}
           </TextField>
-          <a href="/gestionAlerte" onClick={Clear}><Button variant="contained" >Clear</Button></a>
+          <a href="/addAlerte" class="btnAddAlerte"><Button color="success" variant="contained" >Créer une alerte</Button></a>
+          <a href="/gestionAlerte" onClick={Clear}><Button color="warning" variant="contained" >Nettoyer</Button></a>
+          
 
           <div class="alertes-board">
      
@@ -139,11 +143,10 @@ const ListAlerte = () => {
                     
             </CardContent>         
             <CardActions>
-            <Link to={`/modifAlerte/${uneAlerte.idAlerte}`}><Button onClick={() => Update(uneAlerte.idAlerte)}>Modifier</Button></Link>
-                      
-                    <Button align="left" onClick={() => Delete(uneAlerte.idAlerte)}><a href="/gestionAlerte">X</a></Button>
-                  
-    
+            <Link to={`/modifAlerte/${uneAlerte.idAlerte}`}><Button onClick={() => Update(uneAlerte.idAlerte)}>Modifier</Button></Link>         
+                    <Button align="left" onClick={() => Delete(uneAlerte.idAlerte)}><a href="/gestionAlerte"><IconButton aria-label="delete" color="error">
+        <DeleteIcon />
+      </IconButton></a></Button>
                 </CardActions>
               </Card>
                   
